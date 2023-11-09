0e7d2dedcb009f587e29256aa07842fb2c7e5932
BigPattty <86346146+BigPattty@users.noreply.github.com>
2023-11-09 18:43:01 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,28 @@
+import discord
+from discord.ext import commands
+
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(command_prefix = "/", intents = intents)
+
+
+
+async def load_cogs():
+    try:
+        await bot.load_extension("cogs.economycog")
+        #await bot.load_extension("")
+        print("\n"'All cogs have loaded successfully!')
+    except Exception as e:
+        print('\n'f"The following error occured when loading cogs: {e}")
+
+@bot.event
+async def on_ready():
+        print("\n"'Test Bot has connected to Discord!')
+
+        await load_cogs()
+
+        
+
+
+bot.run('MTE3MDg3OTM2Mzc1MTg3MDUwNA.GftR8X.V5VQbMxc1lMaXQGfhdlqL8aiP93lxb2tzFkKio')
