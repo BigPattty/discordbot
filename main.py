584dc690066236075dbb257efc1192d8f57a5486
BigPattty <86346146+BigPattty@users.noreply.github.com>
2023-11-11 20:17:45 +1100

    update to reflex changes to new cogs

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
+#importing modules for discord.py
 import discord
 from discord.ext import commands
 
-
 intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix = "/", intents = intents)
+bot = commands.Bot(command_prefix = commands.when_mentioned_or("$"), intents = intents, help_command = None)
 
 
-
+#loading files from the file "cogs"
 async def load_cogs():
     try:
-        await bot.load_extension("cogs.economycog")
-        #await bot.load_extension("")
+        await bot.load_extension("cogs.help")
         print("\n"'All cogs have loaded successfully!')
     except Exception as e:
-        print('\n'f"The following error occured when loading cogs: {e}")
+        print('\n'f"Issue loading cogs: '\n' *** {e} ***")
 
 @bot.event
 async def on_ready():
-        print("\n"'Test Bot has connected to Discord!')
+        print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
 
         await load_cogs()
 
